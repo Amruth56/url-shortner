a9ac0082745a3f7be3e0ade1bf6d7b477e740347
Amruth <ammumandappa@gmail.com>
2025-11-21 19:17:45 +0530

    .

---
+++ app/api/links/route.js
@@ -0,0 +1,7 @@
+import connectToDatabase from "../../../lib/db";
+import Link from "../../../models/Link";
+import { generateCode } from "../../../lib/geneateCode";
+
+export async function GET(){
+    
+}
--- models/Link.js
+++ models/Link.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const LinkSchema = new mongoose.Schema({
-  code: { type: String, required: true, unique: true, index: true },
-  targetUrl: { type: String, required: true },
-  clicks: { type: Number, default: 0 },
-  lastClicked: { type: Date, default: null },
-  createdAt: { type: Date, default: () => new Date() }
+code: { type: String, required: true, unique: true },
+targetUrl: { type: String, required: true },
+clicks: { type: Number, default: 0 },
+lastClicked: { type: Date, default: null },
+createdAt: { type: Date, default: Date.now }
 });
 
-export default mongoose.models.Link || mongoose.model("Link", LinkSchema);
+export default mongoose.models.Link || mongoose.model('Link', LinkSchema);
