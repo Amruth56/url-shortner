654787a72784fd96cea3b54db132aeacc97f2c21
Amruth <ammumandappa@gmail.com>
2025-12-02 09:03:02 +0530

    deleted the entire project

--- app/api/healthz/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  return new Response(
-    JSON.stringify({ status: 200, message: "API is healthy" }),
-    {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    }
-  );
-}
