a917f25f4992e2928c93e6374b306efd33e09413
Amruth <ammumandappa@gmail.com>
2025-12-02 22:30:29 +0530

    .

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,15 +1,25 @@
 import express from "express";
 const app = express();
 import {nanoid} from "nanoid";
+import dotenv from "dotenv";
+
+dotenv.config("./.env")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.  send("hello world")
 })
 
-app.get('/api/create', (req, res) => {
-    
+app.post('/api/create', (req, res) => {
+    const {url} = req.body;
+    console.log(url);
+    res.send(nanoid(7 ));
 })
 
 app.listen(3000, () => {
 console.log('Server is running on port 3000'); 
 })
+
+39: 56
