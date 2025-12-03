ca04af23c8d8f7f3f36a945de906e3bbd06142b3
Amruth <ammumandappa@gmail.com>
2025-12-03 22:28:37 +0530

    modularising the code

---
+++ BACKEND/src/services/short_url.service.js
@@ -0,0 +1,13 @@
+import { generateNanoId } from "../utils/helper.js";
+import urlSchema from '../models/short_url.model.js';
+
+
+export const createShortUrlService = async(url) => {
+    const shortUrl = await generateNanoId(8)
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl
+    })
+    newUrl.save();
+    return shortUrl;
+}
